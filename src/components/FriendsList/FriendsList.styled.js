9189ecb1c3f6353f05c75053f96c108f519992ad
Kreal11 <kirillshyrokov2@gmail.com>
2023-10-19 23:35:38 +0300

    changed bgC for friendsList

--- src/components/FriendsList/FriendsList.styled.js
+++ src/components/FriendsList/FriendsList.styled.js
@@ -20,20 +20,11 @@ export const StyledItem = styled.li`
   margin-right: auto;
 `;
 
-export const StyledIsOnline = styled.span`
-  background-color: #008000;
+export const StyledOnline = styled.span`
+  background-color: ${({ $yes }) => ($yes ? '#008000' : '#ff0000')};
   display: block;
   width: 10px;
   height: 10px;
   border-radius: 50%;
   margin-right: 10px;
-`;
-
-export const StyledIsNotOnline = styled.span`
-  margin-right: 10px;
-  background-color: #ff0000;
-  display: block;
-  width: 10px;
-  height: 10px;
-  border-radius: 50%;
 `;
--- src/components/FriendsList/OneFriend.jsx
+++ src/components/FriendsList/OneFriend.jsx
@@ -1,16 +1,12 @@
-import {
-  StyledItem,
-  StyledIsNotOnline,
-  StyledIsOnline,
-} from './FriendsList.styled';
+import { StyledItem, StyledOnline } from './FriendsList.styled';
 
 export const OneFriend = ({ avatar, name, isOnline }) => {
   return (
     <StyledItem>
       {isOnline ? (
-        <StyledIsOnline></StyledIsOnline>
+        <StyledOnline $yes></StyledOnline>
       ) : (
-        <StyledIsNotOnline></StyledIsNotOnline>
+        <StyledOnline $no></StyledOnline>
       )}
       <img className="avatar" src={avatar} alt="User avatar" width="48" />
       <p className="name">{name}</p>
