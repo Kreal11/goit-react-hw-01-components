7180b2d7812b2ff3eb338ab3e1b098eaffe9308b
Kreal11 <kirillshyrokov2@gmail.com>
2023-10-19 19:08:59 +0300

    try final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendsList } from './FriendsList/FriendsList';
 import { Transactions } from './TransactionsHistory/TransactionHistory';
+
 import user from '../assets/user.json';
 import data from '../assets/data.json';
 import friends from '../assets/friends.json';
